d431b69296cafe60703d1fe715b113544aa857ff
Slayer <aksuplayz1337@gmail.com>
2021-08-12 17:03:32 +0300

    Show the port that the connection has been opened to

--- src/index.js
+++ src/index.js
@@ -15,10 +15,8 @@ creator.on('debug', (message) => logger.log(message));
 creator.on('warn', (message) => logger.warn(message));
 creator.on('error', (error) => logger.error(error));
 creator.on('synced', () => logger.info('Commands synced!'));
-creator.on('commandRun', (command, _, ctx) =>
-  logger.info(`${ctx.user.username}#${ctx.user.discriminator} (${ctx.user.id}) ran command ${command.commandName}`));
-creator.on('commandRegister', (command) =>
-  logger.info(`Registered command ${command.commandName}`));
+creator.on('commandRun', (command, _, ctx) => logger.info(`${ctx.user.username}#${ctx.user.discriminator} (${ctx.user.id}) ran command ${command.commandName}`));
+creator.on('commandRegister', (command) => logger.info(`Registered command ${command.commandName}`));
 creator.on('commandError', (command, error) => logger.error(`Command ${command.commandName}:`, error));
 
 creator
@@ -26,3 +24,5 @@ creator
   .registerCommandsIn(path.join(__dirname, 'commands'))
   .syncCommands()
   .startServer();
+
+logger.info(`Server running on port ${config.webserver.port}`)
